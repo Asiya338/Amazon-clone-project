b8e00aac8571b60ac39e4f2dcb6bc28c369a62a1
Asiya338 <asiyacse504@gmail.com>
2024-09-10 11:51:44 -0700

    brgin amazon clone

---
+++ amazon.js
@@ -0,0 +1,24 @@
+const slider = document.querySelector(".slider");
+const slides = slider.querySelectorAll("li");
+
+// Store the total number of images
+const slideCount = slides.length;
+let activeSlide = 0;
+
+// To change the images dynamically every
+// 5 Secs, use SetInterval() method
+setInterval(() => {
+  slides[activeSlide].classList.remove("active");
+  activeSlide++;
+  if (activeSlide === slideCount) {
+    activeSlide = 0;
+  }
+  slides[activeSlide].classList.add("active");
+}, 5000);
+
+//navsearch
+
+let s = document.getElementsByClassName("navsearch");
+function getme() {
+  s.classList.add("orange");
+}
